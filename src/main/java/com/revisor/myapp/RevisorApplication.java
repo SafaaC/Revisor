7eb0d7b3caa6464b233b03b62acde2bd9480f2df
SafaaC <safaaacherak@gmail.com>
2024-12-07 15:58:47 +0530

    main class modified

---
+++ src/main/java/com/revisor/myapp/RevisorApplication.java
@@ -0,0 +1,13 @@
+package com.revisor.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisorApplication.class, args);
+	}
+
+}
