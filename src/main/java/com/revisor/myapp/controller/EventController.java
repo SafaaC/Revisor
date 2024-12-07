52abacddfd6e1d12643c8b71da3345c900d02ef7
SafaaC <safaaacherak@gmail.com>
2024-12-07 15:58:07 +0530

    event controller

---
+++ src/main/java/com/revisor/myapp/controller/EventController.java
@@ -0,0 +1,37 @@
+package com.revisor.myapp.controller;
+
+import com.revisor.myapp.model.Event;
+import com.revisor.myapp.service.EventService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        Event createdEvent = eventService.createEvent(event);
+        return ResponseEntity.ok(createdEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Event>> getAllEvents() {
+        List<Event> events = eventService.getAllEvents();
+        return ResponseEntity.ok(events);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long id) {
+        Event event = eventService.getEventById(id);
+        return ResponseEntity.ok(event);
+    }
+}
+
+
+
